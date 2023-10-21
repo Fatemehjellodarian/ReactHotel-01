ef74b8bca5a37a5fcf476a297bd155ad1c581f49
Fatemeh Jellodarian <fatemehjellodarian@gmail.com>
2023-10-21 17:18:06 +0330

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "../src/components/Header/Header";
 import {  Toaster } from "react-hot-toast";
 import LocationList from "./components/LocationList/LocationList";
 import { Routes,Route } from "react-router-dom";
+import AppLayout from "./components/AppLayout/AppLayout";
  
 
 function App() {
@@ -11,6 +12,11 @@ function App() {
       <Header/>
       <Routes>
         <Route path="/" element={<LocationList/>}Route/>
+        <Route path="/hotels" element={<AppLayout/>}>
+          <Route index element={<div>Hotels</div>} />
+          <Route path=":id" element={<div>Single Hotel</div>}/>
+        </Route>
+
       </Routes>
       
     </div>) ;
