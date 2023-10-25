fd2a3fd1327b1841391b0b5a7508bf5a1ad91639
Fatemeh Jellodarian <fatemehjellodarian@gmail.com>
2023-10-25 12:11:37 +0330

    improve the program

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,24 +1,73 @@
+import { useEffect, useState } from "react";
 import { useHotels } from "../Context/HotelsProvider";
-import { MapContainer } from "react-leaflet";
+import { MapContainer, useMap } from "react-leaflet";
 import { TileLayer } from "react-leaflet";
 import { Marker } from "react-leaflet";
 import { Popup } from "react-leaflet";
+import { useSearchParams } from "react-router-dom";
+import useGeoLocation from "../../hooks/useGeoLocation";
+
 
 function Map() {
 const {isLoading,hotels}= useHotels();
+const [mapCenter,setMapCenter] = useState([50,5]);
+const [searchParams,setSearchParams] = useSearchParams();
+const lat = searchParams.get("lat");
+const lng = searchParams.get("lng");
+
+const {
+    isLoading: isLoadingPosition,
+    position: geoLocationPosition,
+    getPosition,
+  } = useGeoLocation();
+
+
+useEffect(()=>{
+    if(lat && lng) setMapCenter([lat,lng]);
+
+},[lat,lng]);
+
+useEffect(() => {
+    if (geoLocationPosition?.lat && geoLocationPosition?.lng)
+      setMapCenter([geoLocationPosition.lat, geoLocationPosition.lng]);
+  }, [geoLocationPosition]);
+
+
+
 
   return (
     <div className="mapContainer">
-          <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+          <MapContainer
+          className="map" 
+          center={mapCenter} 
+          zoom={13} 
+          scrollWheelZoom={true}>
+
+<button onClick={getPosition} className="getLocation">
+          {isLoadingPosition ? "Loading ..." : " Use Your Location"}
+        </button>
+
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
     />
-    <Marker position={position}>
-      <Popup>
-        A pretty CSS3 popup. <br /> Easily customizable.
-      </Popup>
-    </Marker>
+    <ChangeCenter position={mapCenter}  />
+
+    
+    {hotels.map((item)=> (
+
+            <Marker key={item.id} position={[item.latitude  , item.longitude]}>
+            <Popup>
+              {item.host_location}
+            </Popup>
+          </Marker>
+      
+
+        
+    ))}
+
+
+
   </MapContainer>
 
     </div>
@@ -27,3 +76,11 @@ const {isLoading,hotels}= useHotels();
 }
 
 export default Map;
+
+
+function  ChangeCenter({position}){
+ const map =  useMap();
+ map.setView(position);
+ return null ;
+
+}
