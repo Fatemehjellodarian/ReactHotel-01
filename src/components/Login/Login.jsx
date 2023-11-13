3f8c0fde678dcbd03d110b7736309e759cfed041
Fatemeh Jellodarian <fatemehjellodarian@gmail.com>
2023-11-13 12:08:55 +0330

    remove comments

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,7 +3,7 @@ import { useAuth } from "../context/AuthProvider";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
-  const [email, setEmail] = useState("user@gmail.com");
+  const [email, setEmail] = useState("fatemehjellodarian@gmail.com");
   const [password, setPassword] = useState("1234");
   const { login, isAuthenticated } = useAuth();
   const navigate = useNavigate();
